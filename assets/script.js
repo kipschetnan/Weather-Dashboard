520f110ddb21fa8c05bdcc61d7fd819cb767b67c
Kip Schetnan <kipdougschetnan@gmail.com>
2022-10-11 14:41:46 -0500

    added dates to each block that needed and worked on the history

--- assets/script.js
+++ assets/script.js
@@ -25,15 +25,31 @@ var humidity4El = document.getElementById('humidity4');
 var temp5El = document.getElementById('temp5');
 var wind5El = document.getElementById('wind5');
 var humidity5El = document.getElementById('humidity5');
+var geocoderEl = document.getElementById('geocoder');
+var listEl = document.getElementById('list');
+var date1 = document.getElementById('date1');
+var date2 = document.getElementById('date2');
+var date3 = document.getElementById('date3');
+var date4 = document.getElementById('date4');
+var date5 = document.getElementById('date5');
+
+var today = moment();
+tDate = today.format("MM/DD/YY");
+d1Date = today.add(1, 'days').format("MM/DD/YY");
+d2Date = today.add(1, 'days').format("MM/DD/YY");
+d3Date = today.add(1, 'days').format("MM/DD/YY");
+d4Date = today.add(1, 'days').format("MM/DD/YY");
+d5Date = today.add(1, 'days').format("MM/DD/YY");
+
 
 let dataObject;
 let long = '';
 let lat = '';
-let cityName = '';
+let history = [];
 
 
 function weatherDash(data) {
-  cityName = data.city.name;
+  let cityName = data.city.name;
   let temp = data.list[0].main.temp;
   let wind = data.list[0].wind.speed;
   let humidity = data.list[0].main.humidity;
@@ -42,13 +58,16 @@ function weatherDash(data) {
   humidityEl.innerHTML = 'Humidity: ' + humidity + '%';
   windEl.innerHTML = 'Wind: ' + wind + ' MPH';
   tempEl.innerHTML = 'Temp: ' + temp + '°F';
-  locationEl.innerHTML = cityName;
+  locationEl.innerHTML = cityName + ' ' + '(' + tDate + ')';
+  ;
 }
 
 function day1(data) {
   let temp = data.list[11].main.temp;
   let wind = data.list[11].wind.speed;
   let humidity = data.list[11].main.humidity;
+
+  date1.innerHTML = d1Date;
   humidity1El.innerHTML = 'Humidity: ' + humidity + '%';
   wind1El.innerHTML = 'Wind: ' + wind + ' MPH';
   temp1El.innerHTML = 'Temp: ' + temp + '°F';
@@ -59,6 +78,8 @@ function day2(data) {
   let temp = data.list[19].main.temp;
   let wind = data.list[19].wind.speed;
   let humidity = data.list[19].main.humidity;
+
+  date2.innerHTML = d2Date;
   humidity2El.innerHTML = 'Humidity: ' + humidity + '%';
   wind2El.innerHTML = 'Wind: ' + wind + ' MPH';
   temp2El.innerHTML = 'Temp: ' + temp + '°F';
@@ -69,6 +90,8 @@ function day3(data) {
   let temp = data.list[27].main.temp;
   let wind = data.list[27].wind.speed;
   let humidity = data.list[27].main.humidity;
+
+  date3.innerHTML = d3Date;
   humidity3El.innerHTML = 'Humidity: ' + humidity + '%';
   wind3El.innerHTML = 'Wind: ' + wind + ' MPH';
   temp3El.innerHTML = 'Temp: ' + temp + '°F';
@@ -79,6 +102,8 @@ function day4(data) {
   let temp = data.list[35].main.temp;
   let wind = data.list[35].wind.speed;
   let humidity = data.list[35].main.humidity;
+
+  date4.innerHTML = d4Date;
   humidity4El.innerHTML = 'Humidity: ' + humidity + '%';
   wind4El.innerHTML = 'Wind: ' + wind + ' MPH';
   temp4El.innerHTML = 'Temp: ' + temp + '°F';
@@ -89,6 +114,8 @@ function day5(data) {
   let temp = data.list[39].main.temp;
   let wind = data.list[39].wind.speed;
   let humidity = data.list[39].main.humidity;
+
+  date5.innerHTML = d5Date;
   humidity5El.innerHTML = 'Humidity: ' + humidity + '%';
   wind5El.innerHTML = 'Wind: ' + wind + ' MPH';
   temp5El.innerHTML = 'Temp: ' + temp + '°F';
@@ -105,15 +132,27 @@ function getApi() {
     .then(function (data) {
       console.log(data);
       dataObject = data;
-
       weatherDash(dataObject);
       day1(dataObject);
       day2(dataObject);
       day3(dataObject);
       day4(dataObject);
       day5(dataObject);
+      
     
     })
+    
+    localStorage.setItem('list', JSON.stringify(history));
+    var storedHistory = JSON.parse(localStorage.getItem('list'));
+    var li = document.createElement("li");
+
+    for(var i = 0; i < storedHistory.length; i++) {      
+      console.log(storedHistory[i]);
+      li.innerHTML = storedHistory[i];
+      listEl.appendChild(li);
+    }
+
+
   }
 
 const geocoder = new MapboxGeocoder({
@@ -124,8 +163,10 @@ const geocoder = new MapboxGeocoder({
 geocoder.on('result', function(data) {
   long = data.result.geometry.coordinates[0];
   lat = data.result.geometry.coordinates[1];
-
+  history.push(data.result.place_name);
+  console.log(history)
 })
  
+
 geocoder.addTo('#geocoder');
 searchEl.addEventListener('click', getApi);
